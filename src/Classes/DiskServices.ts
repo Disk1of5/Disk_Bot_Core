c32a5526cd94e7d03c06e338ad200f0660b0e0bc
Disk1of5 <disk1of5@gmail.com>
2022-09-06 15:50:26 -0600

    added ExpressjsService

--- src/Classes/DiskServices.ts
+++ src/Classes/DiskServices.ts
@@ -1,18 +1,24 @@
 import { Logger } from "tslog";
 import { DiskConfig } from "./DiskConfig";
+import { ExpressJsService, iExpressJsService } from "./ExpressJsService";
 
 export interface iDiskServices{
     getLogger(): Logger
     getConfig(): DiskConfig
+    getExpressJsService(): iExpressJsService;
 }
 
 export class DiskServices implements iDiskServices{
     private _config : DiskConfig;
     private _logger: Logger;
-    constructor(logger: Logger, config: DiskConfig){
+    private _expressJsService: ExpressJsService;
+    constructor(logger: Logger, expressJsService: ExpressJsService, config: DiskConfig){
         this._logger=logger;
         this._config=config;
-        
+        this._expressJsService=expressJsService;
+    }
+    getExpressJsService(): iExpressJsService {
+        return this._expressJsService;
     }
 
     getLogger(): Logger {
---
+++ src/Classes/ExpressJsService.ts
@@ -0,0 +1,37 @@
+import express, { RequestHandler } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+import { Logger } from "tslog";
+
+export interface iExpressJsService{
+    getService() : express.Application;
+    getPort(): Number;
+    addRoute(path: string, requestHandler: RequestHandler): void;
+}
+export class ExpressJsService implements iExpressJsService {
+    //TODO: to place in configs in .env
+    private _port: number = 8069
+    private _app: express.Application;
+    private _logger: Logger;
+    constructor(logger: Logger){
+        this._logger=logger;
+        this._app=express();
+        this._app.listen(this._port,()=>{
+            this._logger.debug(`Express JS Service Starting on Port ${this._port}`);
+        });                
+    }
+    addRoute(path: string, requestHandler: express.RequestHandler<ParamsDictionary, any, any, ParsedQs, Record<string, any>>): void {
+        this._app.get(path, requestHandler);
+    }
+    getPort(){
+        return this._port
+    };
+    
+    getService(): express.Application {
+        return this._app;
+    }
+
+    
+
+
+}
